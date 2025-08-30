e2b11e762bff1cdbcaa0aedfe5a31ba53c9c4058
oxytoxin <61694153+oxytoxin@users.noreply.github.com>
2025-08-30 16:44:55 +0000

    Fix styling

--- src/Facades/Rewired.php
+++ src/Facades/Rewired.php
@@ -1,16 +1,16 @@
 <?php
 
-    namespace Rewired\Facades;
+namespace Rewired\Facades;
 
-    use Illuminate\Support\Facades\Facade;
+use Illuminate\Support\Facades\Facade;
 
-    /**
-     * @see \Rewired\Rewired
-     */
-    class Rewired extends Facade
+/**
+ * @see \Rewired\Rewired
+ */
+class Rewired extends Facade
+{
+    protected static function getFacadeAccessor(): string
     {
-        protected static function getFacadeAccessor(): string
-        {
-            return \Rewired\Rewired::class;
-        }
+        return \Rewired\Rewired::class;
     }
+}
